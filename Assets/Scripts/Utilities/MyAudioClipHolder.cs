2c7f27704a151a8de7e9e35d054893e913884b15
Stavrin <steffanleprince@gmail.com>
2022-09-29 19:43:39 +0100

    Add files via upload
    Adding MyAudioClipHolder.cs script manually because for some reason Sourcetree will not commit it.

---
+++ Assets/Scripts/Utilities/MyAudioClipHolder.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// System.Serializable lets you change this class in editor
+[System.Serializable]
+public class AudioClipArray
+{
+    // your "inner" array
+    public AudioClip[] clips;
+}
+
+public class MyAudioClipHolder : MonoBehaviour
+{
+    // the "outer" array
+    public AudioClipArray[] clipArrays;
+}
