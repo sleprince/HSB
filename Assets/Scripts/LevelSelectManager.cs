26d8d47e305cea34734cd13b81c448f1e6ffa574
Stavrin <steffanleprince@gmail.com>
2022-11-01 21:10:00 +0000

    Level select screen almost working.

--- Assets/Scripts/LevelSelectManager.cs
+++ Assets/Scripts/LevelSelectManager.cs
@@ -4,29 +4,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 
-public class SelectScreenManager : MonoBehaviour
+public class LevelSelectManager : MonoBehaviour
 {
     public int numberOfPlayers = 1;
-    public List<PlayerInterfaces> plInterfaces = new List<PlayerInterfaces>();
+    public List<LevelInterfaces> LevInterfaces = new List<LevelInterfaces>();
+
+    public List<PlayerInterfaces2> plInterfaces = new List<PlayerInterfaces2>();
+
     int maxRow;
     int maxCollumn;
     List<PotraitInfo> potraitList = new List<PotraitInfo>();
 
     public GameObject potraitCanvas; // the canvas that holds all the potraits
-   
-    bool loadLevel; //if we are loading the level  
+
+    //bool loadLevel; //if we are loading the level  
     public bool bothPlayersSelected; //whether both players chosen and the level will then be loaded.
 
 
-    public static int CharacterName;
+    public static int LevelName;
+
+    Levels levels;
 
     CharacterManager charManager;
 
     GameObject potraitPrefab;
 
     #region Singleton
-    public static SelectScreenManager instance;
-    public static SelectScreenManager GetInstance()
+    public static LevelSelectManager instance;
+    public static LevelSelectManager GetInstance()
     {
         return instance;
     }
@@ -40,13 +45,13 @@ public class SelectScreenManager : MonoBehaviour
     void Start()
     {
         //we start by getting the reference to the character manager
-        charManager = CharacterManager.GetInstance();
-        numberOfPlayers = charManager.numberOfUsers;
+        levels = Levels.GetInstance();
+        numberOfPlayers = levels.numberOfUsers;
 
         potraitPrefab = Resources.Load("potraitPrefab") as GameObject;
         CreatePotraits();
 
-        charManager.solo = (numberOfPlayers == 1);
+       // levels.solo = (numberOfPlayers == 1);
 
     }
 
@@ -58,27 +63,27 @@ public class SelectScreenManager : MonoBehaviour
         int x = 0;
         int y = 0;
 
-        for (int i = 0; i < charManager.characterList.Count; i++)
+        for (int i = 0; i < levels.LevelList.Count; i++)
         {
-            CharacterBase c = charManager.characterList[i];
+            LevelBase c = levels.LevelList[i];
 
             GameObject go = Instantiate(potraitPrefab) as GameObject;
             go.transform.SetParent(potraitCanvas.transform);
 
             PotraitInfo p = go.GetComponent<PotraitInfo>();
             p.img.sprite = c.icon;
-            p.characterId = c.charId;
+            p.characterId = c.levelId;
             p.posX = x;
             p.posY = y;
             potraitList.Add(p);
 
-            if(x < maxRow-1)
+            if (x < maxRow - 1)
             {
                 x++;
             }
             else
             {
-                x=0;
+                x = 0;
                 y++;
             }
 
@@ -88,64 +93,56 @@ public class SelectScreenManager : MonoBehaviour
 
     void Update()
     {
-        if (!loadLevel)
-        {
+
             for (int i = 0; i < plInterfaces.Count; i++)
             {
-                //bringing characterbase info into each player interface as a new characterbase.
-               // plInterfaces[i].characterBase = charManager.characterList[i];
+                //bringing LevelBase info into each player interface as a new LevelBase.
+                // plInterfaces[i].LevelBase = levels.LevelList[i];
 
-                if (i < numberOfPlayers)
-                {
-                    if (Input.GetButtonUp("B" + charManager.players[i].inputId))
-                    {
-                        plInterfaces[i].playerBase.hasCharacter = false;
-                    }
+       
 
-                    if (!charManager.players[i].hasCharacter)
-                    {
+
                         //bringing playerbase info into each player interface as a new playerbase.
-                        plInterfaces[i].playerBase = charManager.players[i];
+                       // plInterfaces[i].playerBase = charManager.players[i];
 
-                        
-                        //plInterfaces[i].characterBase.CharNum = null;
+
+                        //plInterfaces[i].LevelBase.CharNum = null;
                         //plInterfaces[i].CharNum = null;
 
                         HandleSelectorPosition(plInterfaces[i]);
-                        HandleSelectScreenInput(plInterfaces[i], charManager.players[i].inputId);
-                        HandleCharacterPreview(plInterfaces[i]);
-                    }
-                }
-                else
-                {
-                    charManager.players[i].hasCharacter = true;
-                    //trying to play chosen character sound.
-                    //plInterfaces[i].SelectSound.play();
-                    //charManager.characterList.SelectSound.play();
-                }
-            }
-           
-        }
+                        HandleSelectScreenInput(plInterfaces[i], levels.players[i].inputId);
+                        HandleCharacterPreview(plInterfaces[0]);
 
-        if(bothPlayersSelected)
+                    //  }
+
+                        //trying to play chosen character sound.
+                        //plInterfaces[i].SelectSound.play();
+                        //charManager.characterList.SelectSound.play();
+
+
+           }
+
+
+
+        if (bothPlayersSelected)
         {
             Debug.Log("loading");
             StartCoroutine("LoadLevel"); //and start the coroutine to load the level
-            loadLevel = true;
+            //loadLevel = true;
             bothPlayersSelected = false;
         }
         else
         {
-            if(charManager.players[0].hasCharacter 
-                && charManager.players[1].hasCharacter)
+            if (Input.GetButtonDown("A") || Input.GetButtonDown("A1"))
             {
                 bothPlayersSelected = true;
+                LoadLevel();
             }
-           
+
         }
     }
-  
-    void HandleSelectScreenInput(PlayerInterfaces pl, string playerId)
+
+    void HandleSelectScreenInput(PlayerInterfaces2 pl, string playerId)
     {
         #region Grid Navigation
 
@@ -182,19 +179,19 @@ public class SelectScreenManager : MonoBehaviour
             {
                 if (horizontal > 0)
                 {
-                    pl.activeX = (pl.activeX > 0) ? pl.activeX - 1 : maxRow-1;
+                    pl.activeX = (pl.activeX > 0) ? pl.activeX - 1 : maxRow - 1;
                 }
                 else
                 {
-                    pl.activeX = (pl.activeX < maxRow-1) ? pl.activeX + 1 : 0;
+                    pl.activeX = (pl.activeX < maxRow - 1) ? pl.activeX + 1 : 0;
                 }
 
                 pl.timerToReset = 0;
                 pl.hitInputOnce = true;
             }
         }
-       
-        if(vertical == 0 && horizontal == 0)
+
+        if (vertical == 0 && horizontal == 0)
         {
             pl.hitInputOnce = false;
         }
@@ -212,91 +209,19 @@ public class SelectScreenManager : MonoBehaviour
 
         #endregion
 
-        //if the user presses space, he has selected a character
-        if (Input.GetButtonUp("A" + playerId))
-        {
-            //make a reaction on the character, because why not
-            pl.createdCharacter.GetComponentInChildren<Animator>().Play("Kick");
 
-            //play the relevant select sound
-            Completed.SoundManager.instance.RandomizeSfx(pl.SelectSound);
-
-
-            //pass the character to the character manager so that we know what prefab to create in the level
-            pl.playerBase.playerPrefab =
-               charManager.returnCharacterWithID(pl.activePotrait.characterId).prefab;
-
-            //pass the character name to the character manager so that we can use it in the level.
-            pl.playerBase.CharName =
-                charManager.returnCharacterWithID(pl.activePotrait.characterId).CharName;
-
-            //pass the character sounds to the character manager so that we can use them in the level.
-            pl.playerBase.CharSounds =
-    charManager.returnCharacterWithID(pl.activePotrait.characterId).CharSounds;
-
-
-            //trying to make global character identification.
-            if (pl.createdCharacter.name == "Fighter Object(Clone)")
-            {
-                CharacterName = 1;
-                //Debug.Log(CharacterName);
-            }
-
-
-            pl.playerBase.hasCharacter = true;
-        }
+        levels.players[0].hasCharacter = true;
     }
+    
 
-    IEnumerator LoadLevel()
+    void LoadLevel()
     {
-        //if any of the players is an AI, then assign a random character to the prefab
-        for (int i = 0; i < charManager.players.Count; i++)
-        {
-            if(charManager.players[i].playerType == PlayerBase.PlayerType.ai)
-            {
-                if(charManager.players[i].playerPrefab == null)
-                {
-                    int ranValue = Random.Range(0, potraitList.Count);
 
-                    charManager.players[i].playerPrefab = 
-                        charManager.returnCharacterWithID(potraitList[ranValue].characterId).prefab;
-
-                    //pl.CharName is the property we want to bring in to the level to show their name under the health bar.
-                    charManager.players[i].CharName = 
-            CharacterManager.GetInstance().returnCharacterWithID(potraitList[ranValue].characterId).CharName;
-                    //have to bring everything for the ai character too
-
-                    //pass the character sounds to the character manager so that we can use them in the level.
-                    charManager.players[i].CharSounds =
-            charManager.returnCharacterWithID(potraitList[ranValue].characterId).CharSounds;
-
-                    //bringing the character ID number over from the Character list
-                    charManager.players[i].CharNum = potraitList[ranValue].characterId;
-
-
-
-
-                    Debug.Log(potraitList[ranValue].characterId);
-                }
-            }
-        }
-
-        yield return new WaitForSeconds(2);//after 2 seconds load the level
-
-        if (charManager.solo)
-        {
-            MySceneManager.GetInstance().CreateProgression();
-            MySceneManager.GetInstance().LoadNextOnProgression();
-        }
-        else
-        {
-            //here is where I should change it, to loading level select screen for 2Player mode.
-            MySceneManager.GetInstance().RequestLevelLoad(SceneType.prog, "level_select");
-        }
+        MySceneManager.GetInstance().RequestLevelLoad(SceneType.prog, "level_1");
 
     }
 
-    void HandleSelectorPosition(PlayerInterfaces pl)
+    void HandleSelectorPosition(PlayerInterfaces2 pl)
     {
         pl.selector.SetActive(true); //enable the selector
 
@@ -307,7 +232,7 @@ public class SelectScreenManager : MonoBehaviour
             pl.activePotrait = pi; //find the active potrait
 
             //bringing the select sound for which character is being hovered over to the player interface.
-            pl.SelectSound = CharacterManager.GetInstance().returnCharacterWithID(pi.characterId).SelectSound;
+           // pl.SelectSound = CharacterManager.GetInstance().returnCharacterWithID(pi.characterId).SelectSound;
 
             //and place the selector over it's position
             Vector2 selectorPosition = pl.activePotrait.transform.localPosition;
@@ -319,8 +244,9 @@ public class SelectScreenManager : MonoBehaviour
         }
     }
 
-    void HandleCharacterPreview(PlayerInterfaces pl)
+    void HandleCharacterPreview(PlayerInterfaces2 pl)
     {
+
         //if the previews potrait we had, is not the same as the active one we have
         //that means we changed characters
         if (pl.previewPotrait != pl.activePotrait)
@@ -337,7 +263,7 @@ public class SelectScreenManager : MonoBehaviour
             //and create another one
             GameObject go = Instantiate(
                 //character gets made with this function
-                CharacterManager.GetInstance().returnCharacterWithID(pl.activePotrait.characterId).prefab,
+                levels.returnLevelWithID(pl.activePotrait.characterId).prefab,
                 //where you could maybe change the scale
                 pl.charVisPos.position,
                 Quaternion.identity) as GameObject;
@@ -352,15 +278,13 @@ public class SelectScreenManager : MonoBehaviour
             pl.createdCharacter = go;
             //putting character back in the place where it's meant to spawn
             //go.transform.position = pl.charVisPos.position;
-            go.transform.localScale = HalfScale;
+           go.transform.localScale = HalfScale;
 
             pl.previewPotrait = pl.activePotrait;
-
-            if(!string.Equals(pl.playerBase.playerId, charManager.players[0].playerId))
-            {
-                pl.createdCharacter.GetComponent<StateManager>().lookRight = false;
-            }
         }
+
+
+
     }
 
 
@@ -369,7 +293,7 @@ public class SelectScreenManager : MonoBehaviour
         PotraitInfo r = null;
         for (int i = 0; i < potraitList.Count; i++)
         {
-            if(potraitList[i].posX == x && potraitList[i].posY == y)
+            if (potraitList[i].posX == x && potraitList[i].posY == y)
             {
                 r = potraitList[i];
             }
@@ -379,7 +303,36 @@ public class SelectScreenManager : MonoBehaviour
     }
 
     [System.Serializable]
-    public class PlayerInterfaces
+    public class LevelInterfaces
+    {
+        public PotraitInfo activePotrait; //the current active potrait for player 1
+        public PotraitInfo previewPotrait;
+        public GameObject selector; //the select indicator for player1
+        public Transform charVisPos; //the visualization position for player 1
+        public GameObject createdCharacter; // the created character for player 1
+
+        //public AudioClip SelectSound; //select sound for the chosen character
+        public int LevelNum; //character id number for the chosen character
+        public Text LevelName; //character name for the chosen character
+        //public AudioClip[] CharSounds; //character sounds for the chosen character
+
+
+        public int activeX;//the active X and Y entries for player 1
+        public int activeY;
+
+        //variables for smoothing out input
+        public bool hitInputOnce;
+        public float timerToReset;
+
+        //public PlayerBase playerBase;
+        //bringing LevelBase info into each player interface as a new LevelBase.
+        //public LevelBase LevelBase;
+
+        //public LevelBase LevelBase;
+    }
+
+    [System.Serializable]
+    public class PlayerInterfaces2
     {
         public PotraitInfo activePotrait; //the current active potrait for player 1
         public PotraitInfo previewPotrait;
@@ -401,8 +354,8 @@ public class SelectScreenManager : MonoBehaviour
         public float timerToReset;
 
         public PlayerBase playerBase;
-        //bringing characterbase info into each player interface as a new characterbase.
-        public CharacterBase characterBase;
+        //bringing LevelBase info into each player interface as a new LevelBase.
+        public LevelBase LevelBase;
 
 
     }
